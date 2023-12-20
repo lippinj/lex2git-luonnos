ba41cba2ea780f00d14d7c0e99d815acdba3c245
Joonas Lipping <lippinj@outlook.com>
2023-12-20 15:06:16 +0200

    Implement a change

--- package/finlaw/list_form.py
+++ package/finlaw/list_form.py
@@ -18,6 +18,39 @@ class Item:
     number: int | str | None
     text: str | None
 
+    def __repr__(self) -> str:
+        match self.type:
+            case ItemType.Nimike:
+                return f"<# {self.text}>"
+            case ItemType.Osa:
+                return f"<{self.number} OSA: {self.text}>"
+            case ItemType.Luku:
+                return f"<{self.number} luku: {self.text}>"
+            case ItemType.Pykälä:
+                return f"<{self.number} §: {self.text}>"
+            case ItemType.Kappale:
+                return f"<{self.text}>"
+            case ItemType.Kohta:
+                return f"<{self.number}) {self.text}>"
+            case ItemType.Tyhjä:
+                return f"<>"
+            case _:
+                raise NotImplementedError
+
+
+class Address:
+    def __init__(self, spec: tuple|str):
+        if isinstance(spec, str):
+            spec = tuple(int(e) for e in src.split("."))
+
+        assert len(spec) in (1, 2, 3, 4), spec
+        assert any(e >= 1 for e in spec), spec
+
+        self.luku = spec[0]
+        self.pykälä = None if (len(spec) < 2) else spec[1]
+        self.momentti = None if (len(spec) < 3) else spec[2]
+        self.kohta = None if (len(spec) < 4) else spec[3]
+
 
 class ListForm:
     def __init__(self, items: [Item] = None):
@@ -29,40 +62,46 @@ class ListForm:
     def __getitem__(self, ind):
         return self.items[ind]
 
-    def find(self, address) -> (int, int):
-        luku, pykälä, momentti = address
-        start, stop = self._find_luku(luku)
-        if pykälä:
-            start, stop = self._find_pykälä(pykälä, start, stop + 1)
-        if momentti:
-            start, stop = self._find_momentti(momentti, start, stop + 1)
+    def find(self, addr: Address|tuple|str) -> (int, int):
+        addr = addr if isinstance(addr, Address) else Address(addr)
+        start, stop = self._find_luku(addr.luku)
+        if addr.pykälä:
+            start, stop = self._find_pykälä(addr.pykälä, start, stop + 1)
+        if addr.momentti:
+            start, stop = self._find_momentti(addr.momentti, start, stop + 1)
+        if addr.kohta:
+            start, stop = self._find_kohta(addr.kohta, start, stop + 1)
         return (start, stop)
 
-    def repeal(self, address):
-        luku, pykälä, momentti = address
-        if momentti:
-            start, stop = self.find(address)
+    def repeal(self, addr: Address|tuple|str):
+        addr = addr if isinstance(addr, Address) else Address(addr)
+        if addr.kohta:
+            raise NotImplementedError
+        if addr.momentti:
+            start, stop = self.find(addr)
             del self.items[start:stop+1]
             self.items.insert(start, Item(ItemType.Tyhjä, None, None))
-        elif pykälä:
-            start, stop = self.find(pykälä)
+        elif addr.pykälä:
+            start, stop = self.find(addr.pykälä)
             if stop > start:
                 del self.items[start+1:stop+1]
         else:
             raise ValueError("Can't repeal Luku")
 
-    def insert(self, address, item: Item):
-        luku, pykälä, momentti = address
-        if momentti:
-            start, stop = self.find(address)
+    def insert(self, addr: Address|tuple|str, item: Item):
+        addr = addr if isinstance(addr, Address) else Address(addr)
+        if addr.kohta:
+            raise NotImplementedError
+        elif addr.momentti:
+            start, stop = self.find(addr)
             self.items.insert(start, item)
         else:
             raise NotImplementedError
 
-    def change(self, address, item: Item):
-        luku, pykälä, momentti = address
-        if momentti:
-            start, stop = self.find(address)
+    def change(self, addr: Address|tuple|str, item: Item):
+        addr = addr if isinstance(addr, Address) else Address(addr)
+        if addr.kohta or addr.momentti:
+            start, stop = self.find(addr)
             if start < stop:
                 del self.items[start+1:stop+1]
             self.items[start] = item
@@ -127,5 +166,28 @@ class ListForm:
     def _find_momentti_stop(self, begin: int, end: int) -> int:
         for i in range(begin, end):
             if self[i].type in (ItemType.Osa, ItemType.Luku, ItemType.Pykälä, ItemType.Kappale):
+                return i - 1
+        return end - 1
+
+    def _find_kohta(self, n: int, begin: int, end: int) -> (int, int):
+        start = self._find_kohta_start(n, begin, end)
+        stop = self._find_kohta_stop(start + 1, end)
+        assert stop >= start
+        return (start, stop)
+
+    def _find_kohta_start(self, n: int, begin: int, end: int) -> int:
+        k = 1
+        for i in range(begin, end):
+            it = self[i]
+            if it.type == ItemType.Kohta:
+                if k == n:
+                    return i
+                else:
+                    k += 1
+        raise ValueError(f"Could not find kohta #{n}")
+
+    def _find_kohta_stop(self, begin: int, end: int) -> int:
+        for i in range(begin, end):
+            if self[i].type in (ItemType.Osa, ItemType.Luku, ItemType.Pykälä, ItemType.Kappale, ItemType.Kohta):
                 return i - 1
         return end - 1
--- package/test/test_change.py
+++ package/test/test_change.py
@@ -12,3 +12,13 @@ def test_momentin_muuttaminen():
     assert L[516] == Item(ItemType.Kappale, None, "Koulutuksen ajalta opiskelijalla on oikeus koulutuspäivärahaan. Koulutuspäivärahana maksetaan sitä tämän lain mukaista työttömyysetuutta, jota koskevia säännöksiä henkilöön sovellettaisiin hänen työttömänä ollessaan.")
     assert L[517] == item
     assert L[518] == Item(ItemType.Pykälä, 6, "Koulutuspäivärahakauden kesto")
+
+
+def test_kohdan_muuttaminen():
+    L = markdown.load(common.data_path("2002.1290.md"))
+
+    item = Item(ItemType.Kohta, 10, "_työnhakusuunnitelmalla_ julkisesta työvoimapalvelusta annetun lain mukaista työttömän työnhakijan työllistymisedellytysten selvittämiseksi ja niiden parantamiseksi laadittua suunnitelmaa;")
+    L.change((1, 5, 1, 10), item)
+    assert L[27] == Item(ItemType.Kohta, 9, "_työssäolovelvoitteella_ työssä- tai koulutuksessaoloa, joka edellytetään ennen kuin työttömyysetuutta voidaan maksaa toistuvan 8 kohdassa tarkoitetun työhaluttomuutta osoittavan menettelyn johdosta, tai joka edellytetään ennen kuin työmarkkinatukea voidaan maksaa työttömyysajalta 8 luvun 2 §:n 3 momentissa tarkoitetun menettelyn johdosta;")
+    assert L[28] == item
+    assert L[29] == Item(ItemType.Kohta, 11, "_yhdistelmätuella_ pitkään työttömänä olleen työllistymisen edistämiseksi tarkoitettua tukea, jossa työmarkkinatuki voidaan määrätä maksettavaksi työnantajalle joko yksinään tai yhdistettynä julkisesta työvoimapalvelusta annetussa laissa tarkoitettuun työllistämistukeen.")
